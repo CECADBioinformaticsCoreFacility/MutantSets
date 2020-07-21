9e670db5870768a9493b109ba86d1231bfe08d92
RichardJActon <rjactonspsfcf@gmail.com>
2020-07-21 15:56:27 +0200

    basic deletion data support

--- app.R
+++ app.R
@@ -1,23 +1,27 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# This is a Shiny web application. 
 #
 # Notes -----------------------------------------------------------------------
 # Quality Pre-filtering
-# 
+
+# top priority
+# - [ ] deletion filtering
+# - [ ] multi variant loci handling
+# - [ ] extended Quality filtering options
+# - [ ] packageise
+# - [ ] dockerise
+
 # - [ ] set membership plot - which sets
 # - [ ] mutation types proportion
 # - [ ] dropdown button selectize chrs?
-# - [ ] pick columns to show (use $meta)
+# - [x] pick columns to show (use $meta)
 # - [ ] show full gt table in Tab box
 # - [ ] keys?
-# - [ ] wormbase links?
+# - [ ] wormbase links? ~ tricky due to mixed transcript/gene names
+# - [ ] Download button deletion/multivar
 # - [x] name samples - __display bug on null__
 # - [x] colour types in table - match plot
+# - [x] Download button main variant table
 # 
 # Libraries -------------------------------------------------------------------
 library(shiny)
@@ -36,6 +40,7 @@ library(dplyr)
 library(DT)
 library(vcfR)
 library(future)
+library(vroom)
 #library(BSgenome.Celegans.UCSC.ce11)
 #library(TxDb.Celegans.UCSC.ce11.ensGene)
 
@@ -46,7 +51,9 @@ gtfnames <- c(
 	"score", "strand","phase","attributes"
 )
 # seq <- ape::as.DNAbin(getSeq(BSgenome.Celegans.UCSC.ce11))
-# #vcfin <- vcfR::read.vcfR("~/Downloads/Galaxy171-[SnpSift_Filter_on_data_158].vcf", verbose = FALSE)
+# vcfin <- vcfR::read.vcfR(
+# "~/Downloads/Galaxy171-[SnpSift_Filter_on_data_158].vcf", verbose = FALSE
+# )
 
 # ann <- as.data.frame( # Package this data!!
 # 	vroom::vroom(
@@ -65,10 +72,11 @@ var_type_colours <- c(
 )
 
 # Functions -------------------------------------------------------------------
-## |Effect Annotation -------------------------------------------------------
+## |Effect Annotation ---------------------------------------------------------
 
 #' effParser
-#' parsing snpeff strings EFF field http://snpeff.sourceforge.net/SnpEff_manual.html#eff
+#' parsing snpeff strings EFF field 
+#' see: http://snpeff.sourceforge.net/SnpEff_manual.html#eff
 #' @param df a dataframe with an EFF column containing and EFF string
 #' @return a dataframe with columns for 
 effParser <- function(df) {
@@ -92,7 +100,8 @@ effParser <- function(df) {
 }
 
 #' effExtractor
-#' split multiple EFF strings into separate individual EFF strings and convert each to a row in a table
+#' split multiple EFF strings into separate individual EFF strings
+#'  and convert each to a row in a table
 #' @param eff an EFF string or multiple comma separated EFF strings
 #' @return a tibble with an EFF column where each row is a single EFF string
 effExtractor <- function(eff) {
@@ -121,6 +130,45 @@ gtformfun <- function(dt, id) {
 		)
 }
 
+## | Deletion filtering -------------------------------------------------------
+categoriseSample <- function(x, nm) {
+	nm <- gsub("sample_(.*)", "\\1", nm)
+	tb <- NULL
+	if (any(c("1/1", "0/1") %in% x)) {
+		tb <- tibble::tibble_row(sample = nm, keep = TRUE)
+	} else if("0/0" %in% x) {
+		tb <- tibble::tibble_row(sample = nm, keep = FALSE)
+	}
+	return(tb)
+}
+
+delFeatFun <- function(df, sampids, lst) {
+	#lst <- input[paste0("sample_", sampids)]
+	if(all(sapply(lst, is.null))) { return(df) }
+	samples2keep <- map2_dfr(lst, names(lst), ~categoriseSample(.x, .y))
+	df <- df %>% filter(
+		across(
+			matches(
+				paste0(
+					"score_",
+					dplyr::filter(samples2keep, keep == TRUE)$sample
+				)
+			),
+			~ !is.na(.x)
+		),
+		across(
+			matches(
+				paste0(
+					"score_",
+					dplyr::filter(samples2keep, keep == FALSE)$sample
+				)
+			),
+			~is.na(.x)
+		)
+	)
+	return(df)
+}
+
 ## | Plots --------------------------------------------------------------------
 lociPlot <- function(df) {
 	ggplot(df, aes(POS, AF)) + 
@@ -136,14 +184,8 @@ lociPlot <- function(df) {
 			y = "Allele Frequency"
 		)
 }
-# e.g.
-# library(future)
-# plan(multiprocess, workers = 6) 
-# ## vcftidy$dat
-# tmp <- vcftidySel %>% 
-# 	mutate(EFF = furrr::future_map(EFF,effExtractor), nEFF = map_int(EFF,nrow))
 
-## | Chr prep ------------------------------------------------------------------
+## | Chr prep -----------------------------------------------------------------
 chrprep <- function(inobj, min_DP, max_DP, min_MQ, max_MQ, min_QUAL) {
 	chromsMsk <- future.apply::future_lapply(inobj, function(x) {
 		mskd <- masker(
@@ -165,7 +207,7 @@ sidebar <- dashboardSidebar(
 	sidebarMenu(
 		#menuItem("Inputs", tabName = "inputs", icon = icon("upload")),
 		fileInput("vcf", "Select a VCF file", accept = ".vcf"),
-		fileInput("gff (Deletions))", "Select a gff file",accept = ".gff"),
+		fileInput("gff", "Select a gff file", accept = ".gff"),
 		menuItem("Options", tabName = "options", icon = icon("th")),
 		menuItem("Overview", tabName = "overview", icon = icon("th")),
 		menuItem("Filtering", tabName = "table", icon = icon("table"))#,
@@ -223,7 +265,13 @@ body <- dashboardBody(
 						"Allele Frequency",
 						useShinyjs(),
 						extendShinyjs(
-							text = "shinyjs.resetClick = function() { Shiny.onInputChange('.clientValue-plotly_click-A', 'null'); }"
+							text = paste0(
+								"shinyjs.resetClick = function() {",
+									"Shiny.onInputChange(",
+										"'.clientValue-plotly_click-A', 'null'",
+									"); ",
+								"}"
+							)
 						),
 						#plotOutput("chrplot", click = "chrplotclick")#,
 						plotlyOutput("chrplot"),
@@ -247,16 +295,31 @@ body <- dashboardBody(
 				)
 			),
 			fluidRow(
-				box(
-					title = "Variants", 
-					status = "primary", solidHeader = TRUE, collapsible = TRUE,
-					width = 12,
-					#DT::DTOutput("tidytab")
-					#dropdownButton(
+				tabBox(
+					title = "Variants", width = 12,
+					tabPanel(
+						"Variants",
 						uiOutput("col_picker"),
-					#),
-					DT::DTOutput("filtVarsDT")
+						DT::DTOutput("filtVarsDT"),
+						downloadButton("downloadData")
+					),
+					tabPanel(
+						"MiModD deletions",
+						# verbatimTextOutput("test"),
+						DT::DTOutput("filteredDels")
+					)
 				)
+				# box(
+				# 	title = "Variants", 
+				# 	status = "primary", solidHeader = TRUE, collapsible = TRUE,
+				# 	width = 12,
+				# 	#DT::DTOutput("tidytab")
+				# 	#dropdownButton(
+				# 		uiOutput("col_picker"),
+				# 	#),
+				# 	DT::DTOutput("filtVarsDT"),
+				# 	downloadButton("downloadData")
+				# )
 			)
 		)
 	)
@@ -292,7 +355,7 @@ server <- function(input, output) {
 		unique(vcftidy()$dat$CHROM)
 	})
 # Genotype filtering ----------------------------------------------------------
-	## 
+	## set sample names
 	output$renameUI <- renderUI({
 		renamers <- lapply(samples(), function(sampid) {
 			textInput(
@@ -304,13 +367,14 @@ server <- function(input, output) {
 		tagList(renamers)
 	})
 	
+	# Genotype filters UI
 	output$setSelector <- renderUI({
 		selector <- lapply(samples(), function(sampid) {
 			label <- paste0("Genotypes to Include for ", sampid)
-			if(!is.null(input[[paste0("alias_", sampid)]])) {
+			if(!is.null(input[[paste0("alias_", sampid)]])) { # input is not null prior to user setting values?
 				label <- paste0(
 					"Genotypes to Include for ", sampid,
-					" (", input[[paste0("alias_", sampid)]], ")"
+					" (", input[[paste0("alias_", sampid)]], ")" # bug parens are appearing without content
 				)
 			}
 			checkboxGroupButtons(
@@ -327,12 +391,18 @@ server <- function(input, output) {
 		tagList(selector)
 	})
 	
+	multiVarLoci <- reactive({
+		vcftidy()$dat %>%
+			filter(NUMALT > 1)
+	})
+	
 	allLoci <- reactive({
 		nms <- colnames(vcftidy()$dat)
 		gts <- nms[grepl("gt_", nms)]
 		gts <- gts[gts != "gt_GT"]
 		vcftidy()$dat %>%
-			filter(NUMALT <= 1) %>% ###!!!! Need to Handle Multiple Values here!!!
+			###!!!! Need to Handle Multiple Values here!!!
+			filter(NUMALT <= 1) %>% 
 			mutate(AF = as.numeric(AF)) %>%
 			tidyr::unite(pos, CHROM, POS, sep = "_", remove = FALSE) %>%
 			select(-all_of(gts)) %>%
@@ -345,7 +415,8 @@ server <- function(input, output) {
 		req(input$pick_cols)
 		allLoci() %>%
 			select(
-				pos, CHROM, POS, REF, ALT, QUAL,# DP, QR, ODDS, TYPE, NUMALT, EFF, AF,
+				pos, CHROM, POS, REF, ALT, QUAL,
+				# DP, QR, ODDS, TYPE, NUMALT, EFF, AF,
 				all_of(input$pick_cols),
 				EFF,
 				all_of(samples())
@@ -353,7 +424,8 @@ server <- function(input, output) {
 	})
 	
 	tolisten <- reactive({
-		lapply(samples(), function(x){ paste0("sample_", x) })
+		#lapply(samples(), function(x){ paste0("sample_", x) })
+		lapply(samples(), function(x){ input[[paste0("sample_", x)]] })
 	})
 	
 	filtfun <- eventReactive(tolisten(),{
@@ -388,6 +460,14 @@ server <- function(input, output) {
 		#pull(pos)
 	})
 	
+	# |Download data ---------------------------------------------------------
+	output$downloadData <- downloadHandler(
+		filename = "selected_mutants.tsv",
+		content = function(file) {
+			vroom::vroom_write(loci(), file)
+		}
+	)
+	
 	## Chr plot ----------------------------------------
 	#output$testpoints <- renderPrint(print(input$chrplotclick))
 	#output$chrplot <- renderPlot({
@@ -420,8 +500,10 @@ server <- function(input, output) {
 	
 	filtered_vars <- reactive({
 		req(input$filtVarsDT_row_last_clicked)
+		#req(input$filtVarsDT_rows_selected)
 		df <- loci() %>% 
 			dplyr::slice(input$filtVarsDT_row_last_clicked) %>%
+			#dplyr::slice(input$filtVarsDT_rows_selected) %>%
 			pull(EFF) %>%
 			effExtractor() %>%
 			select(-Amino_Acid_Length, -ERRORS, -WARNINGS, -Genotype_Number)
@@ -436,7 +518,9 @@ server <- function(input, output) {
 			effExtractor() %>%
 			select(-Amino_Acid_Length, -ERRORS, -WARNINGS, -Genotype_Number)
 		
-		DT::datatable(df) %>%
+		DT::datatable(
+			df, rownames = FALSE, options = list(scrollX = TRUE),
+		) %>%
 			DT::formatStyle(
 				"Effect", "Effect_Impact", 
 				backgroundColor = DT::styleEqual(
@@ -492,10 +576,11 @@ server <- function(input, output) {
 			#c("#ffeda0", "#feb24c", "#f03b20")
 			c("#efedf5", "#bcbddc", "#756bb1")
 		)(c(0, brks))
-		clrs <- 
-		df %>%
+		
+		clrs <- df %>% 
 			DT::datatable(
 				rownames = FALSE,
+				selection = "single",
 				options = list(
 					#autoWidth = TRUE,
 					#scrollX=TRUE,
@@ -544,21 +629,45 @@ server <- function(input, output) {
 				)
 			) %>%
 			gtformatter()()
-	}, selection = 'single', server = TRUE)
+	}, server = TRUE)
 	
+	# Deletions ---------------------------------------------------------------
 	
 	gff <- reactive({
 		req(input$gff)
-		vroom::vroom(input$gff$datapath, col_names = gtfnames) %>%
+		vroom::vroom(input$gff$datapath, col_names = gtfnames, delim = "\t") %>%
 		as.data.frame() %>% 
 		tidyr::extract(
 			attributes,
 			into = c("sample","p_value"),
 			regex = "sample=(\\d+);p_value=(-?[\\d.]+(?:e-?\\d+)?)",
 			convert = TRUE
-		)
+		) %>%
+		select(-source, -strand, -phase)
 	})
 	
+	gff_wide <- reactive({
+		gff() %>%
+		group_by(sequence, feature, start, end) %>%
+			tidyr::pivot_wider(
+				names_from = sample, values_from = c("score", "p_value")
+			)
+	})
+	
+	genotypeFilterInputs <- eventReactive(tolisten(), {
+		lst <- map(samples(), ~input[[paste0("sample_", .x)]])
+		names(lst) <- paste0("sample_", samples())
+		lst
+	})
+	
+	delTab <- reactive({
+		delFeatFun(gff_wide(), samples(), genotypeFilterInputs())
+	})
+	
+	output$filteredDels <- DT::renderDataTable({
+		#gff() %>% DT::datatable(rownames = FALSE)
+		delTab() %>% DT::datatable(rownames = FALSE)
+	})
 	
 	# output$qualplot <- renderPlot({
 	# 	qualityscores() %>%
@@ -570,10 +679,3 @@ server <- function(input, output) {
 
 # Run the application ---------------------------------------------------------
 shinyApp(ui = ui, server = server)
-
-#chromoqc(do.call("masker",c(chroms[[1]], list(min_QUAL = 1, max_DP = 200)) ),dp.alpha=20)
-
-
-#%>% select(-source, -strand, -phase) %>% 
-# group_by(sequence, feature, start, end) %>%
-# tidyr::pivot_wider(names_from = sample, values_from = c("score", "p_value"))
